ed186cdf6e1320bda26fdaa5b7409bf5a28da6c1
peter <peter@retep.org>
2023-06-07 12:20:02 +0100

    Remove Type and change Ident to allow . references

--- parser/lexer.go
+++ parser/lexer.go
@@ -11,22 +11,16 @@ var (
 		{"hashComment", `#.*`},
 		{"comment", `//.*|/\*.*?\*/`},
 		{"whitespace", `\s+`},
-
-		{"Type", `\b(int|char)\b`},
-		{"Ident", `\b([a-zA-Z_][a-zA-Z0-9_]*)\b`},
+		{"Ident", `\b(([a-zA-Z_][a-zA-Z0-9_]*)(\.([a-zA-Z_][a-zA-Z0-9_]*))*)\b`},
 		{"Punct", `[-,()*/+%{};&!=:<>]|\[|\]`},
 		{"Number", `[-+]?((\d*)?\.\d+|\d+\.(\d*)?)`},
 		{"Int", `[-+]?\d+`},
 		{"String", `"(\\"|[^"])*"`},
-		//{"EOS", `[;\n\r]+`},
-		//{"EOL", `[\n\r]+`},
 	})
 
 	scriptParser = participle.MustBuild[script.Script](
 		participle.Lexer(scriptLexer),
 		participle.UseLookahead(2),
-		//participle.CaseInsensitive("Ident"),
 		participle.Unquote("String"),
-		//participle.Elide("whitespace"),
 	)
 )
