d1aa4058170094d4625fbaa852174dfca61cd253
peter <peter@retep.org>
2023-06-15 19:20:03 +0100

    Add fprint/fprintln

--- stdlib/init.go
+++ stdlib/init.go
@@ -3,6 +3,8 @@ package stdlib
 import "github.com/peter-mount/go-script/executor"
 
 func init() {
+	executor.Register("fprint", _fprint)
+	executor.Register("fprintln", _fprintln)
 	executor.Register("len", _len)
 	executor.Register("map", _map)
 	executor.Register("print", _print)
--- stdlib/print.go
+++ stdlib/print.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"github.com/peter-mount/go-script/executor"
 	"github.com/peter-mount/go-script/script"
+	"io"
 )
 
 func _print(e executor.Executor, call *script.CallFunc, ctx context.Context) error {
@@ -23,4 +24,40 @@ func _println(e executor.Executor, call *script.CallFunc, ctx context.Context) e
 	}
 	fmt.Println(a...)
 	return nil
+}
+
+func _fprint(e executor.Executor, call *script.CallFunc, ctx context.Context) error {
+	a, err := executor.Args(e, call, ctx)
+	if err != nil {
+		return executor.Error(call.Pos, err)
+	}
+
+	if len(a) < 1 {
+		return executor.Errorf(call.Pos, "No args")
+	}
+
+	if w, ok := a[0].(io.Writer); ok {
+		_, err = fmt.Fprint(w, a[1:]...)
+		return err
+	}
+
+	return executor.Errorf(call.Pos, "Expected io.Writer, got %T", a[0])
+}
+
+func _fprintln(e executor.Executor, call *script.CallFunc, ctx context.Context) error {
+	a, err := executor.Args(e, call, ctx)
+	if err != nil {
+		return executor.Error(call.Pos, err)
+	}
+
+	if len(a) < 1 {
+		return executor.Errorf(call.Pos, "No args")
+	}
+
+	if w, ok := a[0].(io.Writer); ok {
+		_, err = fmt.Fprintln(w, a[1:]...)
+		return err
+	}
+
+	return executor.Errorf(call.Pos, "Expected io.Writer, got %T", a[0])
 }
