482b372b726cd5d3b3f130302400214308fef2c8
peter <peter@retep.org>
2023-06-15 19:20:12 +0100

    Include io package

--- tools/goscript/bin/main.go
+++ tools/goscript/bin/main.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"github.com/peter-mount/go-kernel/v2"
 	_ "github.com/peter-mount/go-script/stdlib"
+	_ "github.com/peter-mount/go-script/stdlib/io"
 	_ "github.com/peter-mount/go-script/stdlib/math"
 	_ "github.com/peter-mount/go-script/stdlib/time"
 	"github.com/peter-mount/go-script/tools/goscript"
